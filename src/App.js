213c98be9393829fc680256fe810112d295d8b5e
Ak-Nobelwolf <akshaytadakod@gmail.com>
2023-11-14 20:17:10 +0530

    added some more pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,4 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-
-import Home from "./components/Landing";
-import Experience from "./components/Experience";
-import TechStack from "./components/TechStack";
-import Projects from "./components/Projects";
-import Publications from "./components/Publications";
-import Awards from "./components/Awards";
-import Certificates from "./components/Certificates";
-import Contact from "./components/Contact";
 import Layout from "./components/Layout";
 
 function App() {
