cf2bf1363de8fd6249613bfec734f68aafa1c268
Ak-Nobelwolf <akshaytadakod@gmail.com>
2023-11-14 19:48:04 +0530

    added some more pages

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <>
       <Layout />
       <Routes>
-        <Route path="/" element={<Layout />}>
+        {/* <Route path="/" element={<Layout />}> */}
           <Route path="/" element={<Home />} />
           <Route path="/experience" element={<Experience />} />
           <Route path="/techstack" element={<TechStack />} />
@@ -28,7 +28,7 @@ function App() {
           <Route path="/awards" element={<Awards />} />
           <Route path="/certificates" element={<Certificates />} />
           <Route path="/contact" element={<Contact />} />
-        </Route>
+        {/* </Route> */}
       </Routes>
     </>
   );
