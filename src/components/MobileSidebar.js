9ae8e62e11d8da3fe68a1e38bc404c1b978aae36
Ak-Nobelwolf <akshaytadakod@gmail.com>
2023-11-19 15:54:46 +0530

    Fixed some errors

--- src/components/MobileSidebar.js
+++ src/components/MobileSidebar.js
@@ -1,12 +1,20 @@
 // MobileSidebar.jsx
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faEnvelope, faBriefcase, faCode, faProjectDiagram, faNewspaper, faAward, faCertificate } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faEnvelope,
+  faBriefcase,
+  faCode,
+  faProjectDiagram,
+  faNewspaper,
+  faAward,
+  faCertificate,
+} from "@fortawesome/free-solid-svg-icons";
 import "../styles/MobileSidebar.css";
 
 function MobileSidebar() {
-    console.log("MobileSidebar component rendering...");
   const [isSidebarActive, setSidebarActive] = useState(false);
 
   const handleSidebarToggle = () => {
@@ -14,54 +22,57 @@ function MobileSidebar() {
   };
 
   return (
-    <div className={`mobile-sidebar${isSidebarActive ? " active" : ""}`}>
+    <div className="mobile-sidebar">
       <button className="toggle-button" onClick={handleSidebarToggle}>
         <div className="bar"></div>
         <div className="bar"></div>
         <div className="bar"></div>
       </button>
-      <ul>
-        <li>
-          <NavLink to="/" activeclassName="active-link">
-            <FontAwesomeIcon icon={faHome} color="black" /> Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/experience" activeclassName="active-link">
-            <FontAwesomeIcon icon={faBriefcase} color="black" /> Experience
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/techstack" activeclassName="active-link">
-            <FontAwesomeIcon icon={faCode} color="black" /> TechStack
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/projects" activeclassName="active-link">
-            <FontAwesomeIcon icon={faProjectDiagram} color="black" /> Projects
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/publications" activeclassName="active-link">
-            <FontAwesomeIcon icon={faNewspaper} color="black" /> Publications
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/awards" activeclassName="active-link">
-            <FontAwesomeIcon icon={faAward} color="black" /> Awards
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/certificates" activeclassName="active-link">
-            <FontAwesomeIcon icon={faCertificate} color="black" /> Certificates
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact" activeclassName="active-link">
-            <FontAwesomeIcon icon={faEnvelope} color="black" /> Contact
-          </NavLink>
-        </li>
-      </ul>
+      <div className={`sidebar-content${isSidebarActive ? " active" : ""}`}>
+        <ul>
+          <li>
+            <NavLink to="/" activeclassName="active-link">
+              <FontAwesomeIcon icon={faHome} color="black" /> Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/experience" activeclassName="active-link">
+              <FontAwesomeIcon icon={faBriefcase} color="black" /> Experience
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/techstack" activeclassName="active-link">
+              <FontAwesomeIcon icon={faCode} color="black" /> TechStack
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects" activeclassName="active-link">
+              <FontAwesomeIcon icon={faProjectDiagram} color="black" /> Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/publications" activeclassName="active-link">
+              <FontAwesomeIcon icon={faNewspaper} color="black" /> Publications
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/awards" activeclassName="active-link">
+              <FontAwesomeIcon icon={faAward} color="black" /> Awards
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/certificates" activeclassName="active-link">
+              <FontAwesomeIcon icon={faCertificate} color="black" />{" "}
+              Certificates
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" activeclassName="active-link">
+              <FontAwesomeIcon icon={faEnvelope} color="black" /> Contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,17 @@
 // Sidebar.jsx
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faEnvelope, faBriefcase, faCode, faProjectDiagram, faNewspaper, faAward, faCertificate } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faEnvelope,
+  faBriefcase,
+  faCode,
+  faProjectDiagram,
+  faNewspaper,
+  faAward,
+  faCertificate,
+} from "@fortawesome/free-solid-svg-icons";
 import "../styles/Sidebar.css";
 
 function Sidebar() {
@@ -24,42 +33,42 @@ function Sidebar() {
     >
       <ul>
         <li>
-          <NavLink to="/" activeClassName="active-link">
+          <NavLink to="/" activeclassName="active-link">
             <FontAwesomeIcon icon={faHome} color="black" /> Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/experience" activeClassName="active-link">
+          <NavLink to="/experience" activeclassName="active-link">
             <FontAwesomeIcon icon={faBriefcase} color="Black" /> Experience
           </NavLink>
         </li>
         <li>
-          <NavLink to="/techstack" activeClassName="active-link">
+          <NavLink to="/techstack" activeclassName="active-link">
             <FontAwesomeIcon icon={faCode} color="black" /> TechStack
           </NavLink>
         </li>
         <li>
-          <NavLink to="/projects" activeClassName="active-link">
+          <NavLink to="/projects" activeclassName="active-link">
             <FontAwesomeIcon icon={faProjectDiagram} color="black" /> Projects
           </NavLink>
         </li>
         <li>
-          <NavLink to="/publications" activeClassName="active-link">
+          <NavLink to="/publications" activeclassName="active-link">
             <FontAwesomeIcon icon={faNewspaper} color="black" /> Publications
           </NavLink>
         </li>
         <li>
-          <NavLink to="/awards" activeClassName="active-link">
+          <NavLink to="/awards" activeclassName="active-link">
             <FontAwesomeIcon icon={faAward} color="black" /> Awards
           </NavLink>
         </li>
         <li>
-          <NavLink to="/certificates" activeClassName="active-link">
+          <NavLink to="/certificates" activeclassName="active-link">
             <FontAwesomeIcon icon={faCertificate} color="black" /> Certificates
           </NavLink>
         </li>
         <li>
-          <NavLink to="/contact" activeClassName="active-link">
+          <NavLink to="/contact" activeclassName="active-link">
             <FontAwesomeIcon icon={faEnvelope} color="black" /> Contact
           </NavLink>
         </li>
